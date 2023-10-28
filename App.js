29a0b8f09cddb09d682b5d8f974cfbec6d45ca5a
Carlos Ruiz <carloshruiz90@gmail.com>
2023-10-29 00:48:35 +1000

    Using Redux

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import Main from "./screen/MainComponent";
 import { NavigationContainer } from "@react-navigation/native";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 console.disableYellowBox = true;
 
 export default function App() {
     return (
-      <NavigationContainer>
-          <Main />
-      </NavigationContainer>
+      <Provider store={store}>
+          <NavigationContainer>
+              <Main />
+          </NavigationContainer>
+      </Provider>
     );
 }
 
--- screen/AboutScreen.js
+++ screen/AboutScreen.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
 import { ScrollView, Text } from 'react-native';
-import { Card, ListItem, Avatar } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
+import { Avatar, Card, ListItem } from 'react-native-elements';
+import { useSelector } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
 
 function Mission() {
     return (
@@ -9,29 +9,39 @@ function Mission() {
             <Card.Title>Our Mission</Card.Title>
             <Card.Divider />
             <Text style={{ margin: 10 }}>
-                We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+                We present a curated database of the best campsites in the vast
+                woods and backcountry of the World Wide Web Wilderness. We
+                increase access to adventure for the public while promoting safe
+                and respectful use of resources. The expert wilderness trekkers
+                on our staff personally verify each campsite to make sure that
+                they are up to our standards. We also present a platform for
+                campers to share reviews on campsites they have visited with
+                each other.
             </Text>
         </Card>
     );
 }
 
 const AboutScreen = () => {
-
-    const [partners, setPartners] = useState(PARTNERS);
+    const partners = useSelector((state) => state.partners);
 
     return (
         <ScrollView>
             <Mission />
-            <Card title="Community Partners">
-                {partners.map((partner) => (
+            <Card>
+                <Card.Title>Community Partners</Card.Title>
+                <Card.Divider />
+                {partners.partnersArray.map((partner) => (
                     <ListItem key={partner.id}>
                         <Avatar
                             rounded
-                            source={partner.image}
+                            source={{ uri: baseUrl + partner.image }}
                         />
                         <ListItem.Content>
                             <ListItem.Title>{partner.name}</ListItem.Title>
-                            <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
+                            <ListItem.Subtitle>
+                                {partner.description}
+                            </ListItem.Subtitle>
                         </ListItem.Content>
                     </ListItem>
                 ))}
@@ -41,3 +51,4 @@ const AboutScreen = () => {
 };
 
 export default AboutScreen;
+
